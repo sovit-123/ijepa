411cc48177853f10e6d5728c54982954cb68f957
sovit-123 <sovitrath5@gmail.com>
2025-07-01 19:17:39 +0530

    Refactored engine.py for segmentation training

--- src/img_seg/engine.py
+++ src/img_seg/engine.py
@@ -1,116 +1,134 @@
 import torch
 import torch.nn as nn
 
+from contextlib import nullcontext
 from tqdm import tqdm
+from typing import List, Optional
+
 from src.img_seg.utils import draw_translucent_seg_maps
 from src.img_seg.metrics import IOUEval
 
-def train(
-    model,
-    train_dataloader,
-    device,
-    optimizer,
-    criterion,
-    classes_to_train
+def _run_one_epoch(
+    model: nn.Module,
+    dataloader: torch.utils.data.DataLoader,
+    device: torch.device,
+    criterion: nn.Module,
+    num_classes: int,
+    is_train: bool,
+    optimizer: Optional[torch.optim.Optimizer] = None,
+    epoch: Optional[int] = None,
+    save_dir: Optional[str] = None,
+    label_colors_list: Optional[List] = None,
+    viz_map: Optional[List] = None,
 ):
-    print('Training')
-    model.train()
-    train_running_loss = 0.0
+    """
+    Runs one epoch of training or validation.
+
+    :param model: The neural network model.
+    :param dataloader: The DataLoader for the current epoch.
+    :param device: The computation device.
+    :param criterion: The loss function.
+    :param num_classes: Number of classes for segmentation.
+    :param is_train: Boolean, True if it's a training epoch, False for validation.
+    :param optimizer: The optimizer for training. Required if is_train is True.
+    :param epoch: The current epoch number. Required for validation visualization.
+    :param save_dir: Directory to save validation visualizations.
+    :param label_colors_list: List of colors for labels.
+    :param viz_map: Visualization map for segmentation.
+    """
+    model.train() if is_train else model.eval()
+
+    running_loss = 0.0
+    iou_eval = IOUEval(num_classes)
     prog_bar = tqdm(
-        train_dataloader, 
-        total=len(train_dataloader), 
+        dataloader,
+        total=len(dataloader),
         bar_format='{l_bar}{bar:20}{r_bar}{bar:-20b}'
     )
-    counter = 0 # to keep track of batch counter
-    num_classes = len(classes_to_train)
-    iou_eval = IOUEval(num_classes)
 
-    for i, data in enumerate(prog_bar):
-        counter += 1
-        pixel_values, target = data[0].to(device), data[1].to(device)
-        optimizer.zero_grad()
-        outputs = model(pixel_values)
+    context = nullcontext() if is_train else torch.no_grad()
 
-        upsampled_logits = nn.functional.interpolate(
-                outputs, size=target.shape[-2:], 
-                mode="bilinear", 
-                align_corners=False
-        )
-
-        ##### BATCH-WISE LOSS #####
-        loss = criterion(upsampled_logits, target)
-        train_running_loss += loss.item()
-        ###########################
- 
-        ##### BACKPROPAGATION AND PARAMETER UPDATION #####
-        loss.backward()
-        optimizer.step()
-        ##################################################
-
-        iou_eval.addBatch(upsampled_logits.max(1)[1].data, target.data)
-        
-    ##### PER EPOCH LOSS #####
-    train_loss = train_running_loss / counter
-    ##########################
-    overall_acc, per_class_acc, per_class_iou, mIOU = iou_eval.getMetric()
-    return train_loss, overall_acc, mIOU
-
-def validate(
-    model,
-    valid_dataloader,
-    device,
-    criterion,
-    classes_to_train,
-    label_colors_list,
-    epoch,
-    save_dir,
-    viz_map
-):
-    print('Validating')
-    model.eval()
-    valid_running_loss = 0.0
-    num_classes = len(classes_to_train)
-    iou_eval = IOUEval(num_classes)
-
-    with torch.no_grad():
-        prog_bar = tqdm(
-            valid_dataloader, 
-            total=(len(valid_dataloader)), 
-            bar_format='{l_bar}{bar:20}{r_bar}{bar:-20b}'
-        )
-        counter = 0 # To keep track of batch counter.
+    with context:
         for i, data in enumerate(prog_bar):
-            counter += 1
             pixel_values, target = data[0].to(device), data[1].to(device)
+
+            if is_train:
+                assert optimizer is not None, "Optimizer must be provided for training"
+                optimizer.zero_grad()
+
             outputs = model(pixel_values)
 
             upsampled_logits = nn.functional.interpolate(
-                outputs, size=target.shape[-2:], 
-                mode="bilinear", 
+                outputs, size=target.shape[-2:],
+                mode="bilinear",
                 align_corners=False
             )
-            
-            # Save the validation segmentation maps.
-            if i == 1:
+
+            loss = criterion(upsampled_logits, target)
+            running_loss += loss.item()
+
+            if is_train:
+                loss.backward()
+                optimizer.step()
+
+            iou_eval.addBatch(upsampled_logits.max(1)[1].data, target.data)
+
+            # For validation, save the first batch's segmentation map.
+            if not is_train and i == 0 and all(p is not None for p in [epoch, save_dir, label_colors_list, viz_map]):
                 draw_translucent_seg_maps(
-                    pixel_values, 
-                    upsampled_logits, 
-                    epoch, 
-                    i, 
-                    save_dir, 
+                    pixel_values,
+                    upsampled_logits,
+                    epoch,
+                    i,
+                    save_dir,
                     label_colors_list,
                     viz_map
                 )
 
-            ##### BATCH-WISE LOSS #####
-            loss = criterion(upsampled_logits, target)
-            valid_running_loss += loss.item()
-            ###########################
+    epoch_loss = running_loss / len(dataloader)
+    overall_acc, _, _, mIOU = iou_eval.getMetric()
+    return epoch_loss, overall_acc, mIOU
 
-            iou_eval.addBatch(upsampled_logits.max(1)[1].data, target.data)
-        
-    ##### PER EPOCH LOSS #####
-    valid_loss = valid_running_loss / counter
-    ##########################
-    overall_acc, per_class_acc, per_class_iou, mIOU = iou_eval.getMetric()
-    return valid_loss, overall_acc, mIOU
+def train(
+    model: nn.Module,
+    train_dataloader: torch.utils.data.DataLoader,
+    device: torch.device,
+    optimizer: torch.optim.Optimizer,
+    criterion: nn.Module,
+    classes_to_train: List[str]
+):
+    print('Training')
+    return _run_one_epoch(
+        model=model,
+        dataloader=train_dataloader,
+        device=device,
+        criterion=criterion,
+        num_classes=len(classes_to_train),
+        is_train=True,
+        optimizer=optimizer
+    )
+
+def validate(
+    model: nn.Module,
+    valid_dataloader: torch.utils.data.DataLoader,
+    device: torch.device,
+    criterion: nn.Module,
+    classes_to_train: List[str],
+    label_colors_list: List,
+    epoch: int,
+    save_dir: str,
+    viz_map: List
+):
+    print('Validating')
+    return _run_one_epoch(
+        model=model,
+        dataloader=valid_dataloader,
+        device=device,
+        criterion=criterion,
+        num_classes=len(classes_to_train),
+        is_train=False,
+        epoch=epoch,
+        save_dir=save_dir,
+        label_colors_list=label_colors_list,
+        viz_map=viz_map
+    )
